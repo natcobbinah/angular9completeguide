a79fde24e95717bf8fb13bd74a250f8257d06724
natcobbinah <nathaniel.cobbinah@amalitech.org>
2020-11-27 21:31:21 +0000

    Add inserting,increase and decrese votes, and sorting to the reddit like platform app

---
+++ src/app/article/article.model.ts
@@ -0,0 +1,28 @@
+class Article{
+    title:string;
+    link: string;
+    votes:number;
+
+    constructor(title:string,link:string,votes?:number){
+        this.title = title;
+        this.link = link;
+        this.votes = votes || 0;
+    }
+
+    voteUp():void{
+        this.votes += 1;
+    }
+
+    voteDown():void{
+        this.votes -= 1;
+    }
+
+    domain():string{
+        const domainAndPath:string = this.link.split('//')[1];
+        return domainAndPath.split('/')[0];
+    }catch(err){
+        return null;
+    }
+}
+
+export {Article};
